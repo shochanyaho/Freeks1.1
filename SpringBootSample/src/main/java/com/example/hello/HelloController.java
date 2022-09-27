a96080d4c6d76a5f4b8d6b6117deda762a6426fe
shochanyaho <78130521+shochanyaho@users.noreply.github.com>
2022-09-27 22:23:18 +0900

    test

--- SpringBootSample/src/main/java/com/example/hello/HelloController.java
+++ SpringBootSample/src/main/java/com/example/hello/HelloController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("/hello")
-    public String getHello() {
-        // hello.htmlに画面遷移
-        return "hello";
-    }
-
-    @PostMapping("/hello")
-    public String postRequest(@RequestParam("text1") String str, Model model) {
-
-        // 画面から受け取った文字列をModelに登録
-        model.addAttribute("sample", str);
-
-        // response.htmlに画面遷移
-        return "hello/response";
-    }
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		// hello.htmlに画面遷移
+		return "hello";
+	}
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		
+		// 画面から受け取った文字列をModelに登録
+		model.addAttribute("sample", str);
+		
+		// response.htmlに画面遷移
+		return "hello/response";
+	}
 }
